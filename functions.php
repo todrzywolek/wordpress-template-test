40461ba7c37702b4506906fffbdf36c7da5ab820
Tomasz Odrzywolek <tomeko92@gmail.com>
2017-11-11 20:17:14 +0100

    add header and footer navigation menu

--- functions.php
+++ functions.php
@@ -7,4 +7,10 @@
 
     add_action('wp_enqueue_scripts', 'learningWordPress_resources');
 
+    //Navigaton menus
+    register_nav_menus(array(
+        'primary' => __( 'Primary Menu' ),
+        'footer' => __( 'Footer Menu' )
+    ));
+
 ?>
