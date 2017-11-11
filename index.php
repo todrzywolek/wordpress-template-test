1bfe4735cb109cb2ad99bc7e5650514d928ffe71
Tomasz Odrzywolek <tomeko92@gmail.com>
2017-11-11 19:26:09 +0100

    wrap post into article tag style article

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
   
   if (have_posts()) :
     while (have_posts()) : the_post(); ?>
-      <h2><?php the_title(); ?></h2>
-      <?php the_content(); ?>
+      <article class="post">
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <?php the_content(); ?>
+      </article>
 <?php
     endwhile; 
   else :
