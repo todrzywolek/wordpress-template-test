51f793fe896c36591265c642900657698ae3ab28
Tomasz Odrzywolek <tomeko92@gmail.com>
2017-11-11 18:32:25 +0100

    add container to html

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
-<footer class="site-footer">
-    <p><?php bloginfo('name'); ?> &copy; <?php echo date('Y'); ?></p>
-</footer>
+    <footer class="site-footer">
+        <p><?php bloginfo('name'); ?> &copy; <?php echo date('Y'); ?></p>
+    </footer>
+
+</div>
 
 <?php wp_footer(); ?>
 </body>
